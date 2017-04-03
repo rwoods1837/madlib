58e5c59c3aacd03620c28770beef4d9a72c0acc7
rwoods1837 <rwoods1837@greenvilleschools.us>
2017-04-03 09:29:57 -0400

    Add files via upload

---
+++ story_maker.js
@@ -0,0 +1,29 @@
+function makeStory() {
+    // get form values (2 points)
+    var adjective = document.getElementById('adjective').value;
+    var celebrity = document.getElementById('celebrity').value;
+    var lastname1 = document.getElementById('last-name1').value;
+    var lastname2 = document.getElementById('last-name2').value;
+    var noun1 = document.getElementById('noun1').value;
+    var noun2 = document.getElementById('noun2').value;
+    var pluralanimal = document.getElementById('plural-animal').value;
+    var pluralnoun1 = document.getElementById('plural-noun1').value;
+    var pluralnoun2 = document.getElementById('plural-noun2').value;
+    var pluralnoun3 = document.getElementById('plural-noun3').value;
+    var pluralnoun4 = document.getElementById('plural-noun4').value;
+    var pluralnoun5 = document.getElementById('plural-noun5').value;
+
+
+    // Set title of story. Use at least one form value in the title. (2 points)
+    var title = "Rock Music";
+
+
+    // Build story. you can add as many paragraphs as you like. (3 points)
+    var paragraph1 = "Young people today would rather listen to a good rock music concert than to Johann Sebastian " + lastname1 + " or to Ludvig von " + lastname2 + " Rock music is played by " + adjective + " groups of young men who wear their hair below their " + pluralnoun1 + " They also wear very odd and colorful " + pluralnoun2 + " and often have beards.The groups have attractive names such as 'The " + pluralanimal + " or " + celebrity + " and The Three " + pluralnoun3 + " . They usually play electric. " + plualnoun4 + " One member of the group usually sits on a raised platform and sets the rhythm by beating his " + noun1 + " . The songs are mostly about some fellow who has been rejected by his " + noun2 + " . They are very sad and when young girls hear them, they often get tears in their " +  pluralnoun5 + " .";
+
+
+
+    // Display story by putting title & paragraphs into appropriate divs. (3 points)
+
+
+}
